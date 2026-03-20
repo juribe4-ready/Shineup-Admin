0c940a173d64a01a1a974b47be28695c19562f0b
juribe4-ready <juribe4@gmail.com>
2026-03-20 02:22:32 -0400

    Add files via upload

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_KEY: string
   readonly VITE_AIRTABLE_TOKEN: string
+  readonly VITE_GOOGLE_MAPS_KEY: string
 }
 
 interface ImportMeta {
